sujithshankar/NetworkManager

fixup! core: let device plugins advertise supported link and setting types
Move the factory logic and tracking in nm-device-factory.c

src/devices/nm-device-factory.c
@@ -18,7 +18,14 @@
  * Copyright (C) 2014 Red Hat, Inc.
  */
 
+#include <config.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <string.h>
+
 #include "nm-device-factory.h"
+#include "nm-logging.h"
 
 enum {
 	DEVICE_ADDED,
@@ -26,21 +33,6 @@ enum {
 	LAST_SIGNAL
 };
 static guint signals[LAST_SIGNAL] = { 0 };
-
-static GSList *internal_types = NULL;
-
-void
-_nm_device_factory_internal_register_type (GType factory_type)
-{
-	g_return_if_fail (g_slist_find (internal_types, GUINT_TO_POINTER (factory_type)) == NULL);
-	internal_types = g_slist_prepend (internal_types, GUINT_TO_POINTER (factory_type));
-}
-
-const GSList *
-nm_device_factory_get_internal_factory_types (void)
-{
-	return internal_types;
-}
 
 gboolean
 nm_device_factory_emit_component_added (NMDeviceFactory *factory, GObject *component)
@@ -156,5 +148,261 @@ nm_device_factory_create_virtual_device_for_connection (NMDeviceFactory *factory
 	if (interface->create_virtual_device_for_connection)
 		return interface->create_virtual_device_for_connection (factory, connection, parent, error);
 	return NULL;
+}
+
+/*******************************************************************/
+
+static GSList *internal_types = NULL;
+static GHashTable *factories_by_link = NULL;
+static GHashTable *factories_by_setting = NULL;
+
+void
+_nm_device_factory_internal_register_type (GType factory_type)
+{
+	g_return_if_fail (g_slist_find (internal_types, GUINT_TO_POINTER (factory_type)) == NULL);
+	internal_types = g_slist_prepend (internal_types, GUINT_TO_POINTER (factory_type));
+}
+
+const GSList *
+nm_device_factory_get_internal_factory_types (void)
+{
+	return internal_types;
+}
+
+static void __attribute__((destructor))
+_cleanup (void)
+{
+	g_slist_free (internal_types);
+	g_hash_table_destroy (factories_by_link);
+	g_hash_table_destroy (factories_by_setting);
+}
+
+NMDeviceFactory *
+nm_device_factory_manager_find_factory (const NMLinkType *needle_link_types,
+                                        const char **needle_setting_types)
+{
+	NMDeviceFactory *found;
+	guint i;
+
+	/* NMLinkType search */
+	for (i = 0; needle_link_types[i] > NM_LINK_TYPE_UNKNOWN; i++) {
+		found = g_hash_table_lookup (factories_by_link, GUINT_TO_POINTER (needle_link_types[i]));
+		if (found)
+			return found;
+	}
+
+	/* NMSetting name search */
+	for (i = 0; needle_setting_types[i]; i++) {
+		found = g_hash_table_lookup (factories_by_setting, needle_setting_types[i]);
+		if (found)
+			return found;
+	}
+
+	return NULL;
+}
+
+NMDeviceFactory *
+nm_device_factory_manager_find_factory_for_connection (NMConnection *connection)
+{
+	const char *stypes[2] = { nm_connection_get_connection_type (connection), NULL };
+
+	g_assert (stypes[0]);
+	return nm_device_factory_manager_find_factory (NULL, stypes);
+}
+
+#define PLUGIN_PREFIX "libnm-device-plugin-"
+#define PLUGIN_PATH_TAG "NMManager-plugin-path"
+
+struct read_device_factory_paths_data {
+	char *path;
+	struct stat st;
+};
+
+static gint
+read_device_factory_paths_sort_fcn (gconstpointer a, gconstpointer b)
+{
+	const struct read_device_factory_paths_data *da = a;
+	const struct read_device_factory_paths_data *db = b;
+	time_t ta, tb;
+
+	ta = MAX (da->st.st_mtime, da->st.st_ctime);
+	tb = MAX (db->st.st_mtime, db->st.st_ctime);
+
+	if (ta < tb)
+		return 1;
+	if (ta > tb)
+		return -1;
+	return 0;
+}
+
+static char**
+read_device_factory_paths ()
+{
+	GDir *dir;
+	GError *error = NULL;
+	const char *item;
+	GArray *paths;
+	char **result;
+	guint i;
+
+	dir = g_dir_open (NMPLUGINDIR, 0, &error);
+	if (!dir) {
+		nm_log_warn (LOGD_HW, "device plugin: failed to open directory %s: %s",
+		             NMPLUGINDIR,
+		             (error && error->message) ? error->message : "(unknown)");
+		g_clear_error (&error);
+		return NULL;
+	}
+
+	paths = g_array_new (FALSE, FALSE, sizeof (struct read_device_factory_paths_data));
+
+	while ((item = g_dir_read_name (dir))) {
+		int errsv;
+		struct read_device_factory_paths_data data;
+
+		if (!g_str_has_prefix (item, PLUGIN_PREFIX))
+			continue;
+		if (g_str_has_suffix (item, ".la"))
+			continue;
+
+		data.path = g_build_filename (NMPLUGINDIR, item, NULL);
+
+		if (stat (data.path, &data.st) != 0) {
+			errsv = errno;
+			nm_log_warn (LOGD_HW, "device plugin: skip invalid file %s (error during stat: %s)", data.path, strerror (errsv));
+			goto NEXT;
+		}
+		if (!S_ISREG (data.st.st_mode))
+			goto NEXT;
+		if (data.st.st_uid != 0) {
+			nm_log_warn (LOGD_HW, "device plugin: skip invalid file %s (file must be owned by root)", data.path);
+			goto NEXT;
+		}
+		if (data.st.st_mode & (S_IWGRP | S_IWOTH | S_ISUID)) {
+			nm_log_warn (LOGD_HW, "device plugin: skip invalid file %s (invalid file permissions)", data.path);
+			goto NEXT;
+		}
+
+		g_array_append_val (paths, data);
+		continue;
+NEXT:
+		g_free (data.path);
+	}
+	g_dir_close (dir);
+
+	/* sort filenames by modification time. */
+	g_array_sort (paths, read_device_factory_paths_sort_fcn);
+
+	result = g_new (char *, paths->len + 1);
+	for (i = 0; i < paths->len; i++)
+		result[i] = g_array_index (paths, struct read_device_factory_paths_data, i).path;
+	result[i] = NULL;
+
+	g_array_free (paths, TRUE);
+	return result;
+}
+
+static gboolean
+_add_factory (NMDeviceFactory *factory,
+              gboolean check_duplicates,
+              const char *path,
+              NMDeviceFactoryManagerFactoryLoadedFunc loaded_func,
+              gpointer user_data)
+{
+	NMDeviceFactory *found = NULL;
+	const NMLinkType *link_types = NULL;
+	const char **setting_types = NULL;
+	int i;
+
+	nm_device_factory_get_supported_types (factory, &link_types, &setting_types);
+	if (check_duplicates) {
+		found = nm_device_factory_manager_find_factory (link_types, setting_types);
+		if (found) {
+			nm_log_warn (LOGD_HW, "Loading device plugin failed: multiple plugins "
+			             "for same type (using '%s' instead of '%s')",
+				         (char *) g_object_get_data (G_OBJECT (found), PLUGIN_PATH_TAG),
+				         path);
+			return FALSE;
+		}
+	}
+
+	g_object_set_data_full (G_OBJECT (factory), PLUGIN_PATH_TAG, g_strdup (path), g_free);
+	for (i = 0; link_types[i] > NM_LINK_TYPE_UNKNOWN; i++)
+		g_hash_table_insert (factories_by_link, GUINT_TO_POINTER (link_types[i]), g_object_ref (factory));
+	for (i = 0; setting_types[i]; i++)
+		g_hash_table_insert (factories_by_setting, (char *) setting_types[i], g_object_ref (factory));
+
+	loaded_func (factory, user_data);
+
+	nm_log_info (LOGD_HW, "Loaded device plugin: %s (%s)", G_OBJECT_TYPE_NAME (factory), path);
+	return TRUE;
+}
+
+void
+nm_device_factory_manager_load_factories (NMDeviceFactoryManagerFactoryLoadedFunc loaded_func,
+                                          gpointer user_data)
+{
+	NMDeviceFactory *factory;
+	const GSList *iter;
+	GError *error = NULL;
+	char **path, **paths;
+
+	g_return_val_if_fail (factories_by_link == NULL, FALSE);
+	g_return_val_if_fail (factories_by_setting == NULL, FALSE);
+	factories_by_link = g_hash_table_new_full (g_direct_hash, g_direct_equal, NULL, g_object_unref);
+	factories_by_setting = g_hash_table_new_full (g_str_hash, g_str_equal, NULL, g_object_unref);
+
+	/* Register internal factories first */
+	for (iter = internal_types; iter; iter = iter->next) {
+		GType ftype = (GType) GPOINTER_TO_SIZE (iter->data);
+
+		factory = (NMDeviceFactory *) g_object_new (ftype, NULL);
+		g_assert (factory);
+		_add_factory (factory, FALSE, "internal", loaded_func, user_data);
+	}
+
+	paths = read_device_factory_paths ();
+	if (!paths)
+		return;
+
+	for (path = paths; *path; path++) {
+		GModule *plugin;
+		NMDeviceFactoryCreateFunc create_func;
+		const char *item;
+
+		item = strrchr (*path, '/');
+		g_assert (item);
+
+		plugin = g_module_open (*path, G_MODULE_BIND_LOCAL);
+
+		if (!plugin) {
+			nm_log_warn (LOGD_HW, "(%s): failed to load plugin: %s", item, g_module_error ());
+			continue;
+		}
+
+		if (!g_module_symbol (plugin, "nm_device_factory_create", (gpointer) &create_func)) {
+			nm_log_warn (LOGD_HW, "(%s): failed to find device factory creator: %s", item, g_module_error ());
+			g_module_close (plugin);
+			continue;
+		}
+
+		factory = create_func (&error);
+		if (!factory) {
+			nm_log_warn (LOGD_HW, "(%s): failed to initialize device factory: %s",
+			             item, error ? error->message : "unknown");
+			g_clear_error (&error);
+			g_module_close (plugin);
+			continue;
+		}
+		g_clear_error (&error);
+
+		if (_add_factory (factory, TRUE, g_module_name (plugin), loaded_func, user_data))
+			g_module_make_resident (plugin);
+		else
+			g_module_close (plugin);
+
+		g_object_unref (factory);
+	}
+	g_strfreev (paths);
 }
 

freedesktop-unofficial-mirror/NetworkManager__NetworkManager

src/devices/nm-device-factory.c
@@ -19,8 +19,15 @@
  */
 
 #include "config.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <string.h>
+
+#include <gmodule.h>
 
 #include "nm-device-factory.h"
+#include "nm-logging.h"
 
 enum {
 	DEVICE_ADDED,
@@ -28,21 +35,6 @@ enum {
 	LAST_SIGNAL
 };
 static guint signals[LAST_SIGNAL] = { 0 };
-
-static GSList *internal_types = NULL;
-
-void
-_nm_device_factory_internal_register_type (GType factory_type)
-{
-	g_return_if_fail (g_slist_find (internal_types, GUINT_TO_POINTER (factory_type)) == NULL);
-	internal_types = g_slist_prepend (internal_types, GUINT_TO_POINTER (factory_type));
-}
-
-const GSList *
-nm_device_factory_get_internal_factory_types (void)
-{
-	return internal_types;
-}
 
 gboolean
 nm_device_factory_emit_component_added (NMDeviceFactory *factory, GObject *component)
@@ -158,5 +150,261 @@ nm_device_factory_create_virtual_device_for_connection (NMDeviceFactory *factory
 	if (interface->create_virtual_device_for_connection)
 		return interface->create_virtual_device_for_connection (factory, connection, parent, error);
 	return NULL;
+}
+
+/*******************************************************************/
+
+static GSList *internal_types = NULL;
+static GHashTable *factories_by_link = NULL;
+static GHashTable *factories_by_setting = NULL;
+
+void
+_nm_device_factory_internal_register_type (GType factory_type)
+{
+	g_return_if_fail (g_slist_find (internal_types, GUINT_TO_POINTER (factory_type)) == NULL);
+	internal_types = g_slist_prepend (internal_types, GUINT_TO_POINTER (factory_type));
+}
+
+const GSList *
+nm_device_factory_get_internal_factory_types (void)
+{
+	return internal_types;
+}
+
+static void __attribute__((destructor))
+_cleanup (void)
+{
+	g_slist_free (internal_types);
+	g_hash_table_destroy (factories_by_link);
+	g_hash_table_destroy (factories_by_setting);
+}
+
+NMDeviceFactory *
+nm_device_factory_manager_find_factory (const NMLinkType *needle_link_types,
+                                        const char **needle_setting_types)
+{
+	NMDeviceFactory *found;
+	guint i;
+
+	/* NMLinkType search */
+	for (i = 0; needle_link_types && needle_link_types[i] > NM_LINK_TYPE_UNKNOWN; i++) {
+		found = g_hash_table_lookup (factories_by_link, GUINT_TO_POINTER (needle_link_types[i]));
+		if (found)
+			return found;
+	}
+
+	/* NMSetting name search */
+	for (i = 0; needle_setting_types && needle_setting_types[i]; i++) {
+		found = g_hash_table_lookup (factories_by_setting, needle_setting_types[i]);
+		if (found)
+			return found;
+	}
+
+	return NULL;
+}
+
+NMDeviceFactory *
+nm_device_factory_manager_find_factory_for_connection (NMConnection *connection)
+{
+	const char *stypes[2] = { nm_connection_get_connection_type (connection), NULL };
+
+	g_assert (stypes[0]);
+	return nm_device_factory_manager_find_factory (NULL, stypes);
+}
+
+#define PLUGIN_PREFIX "libnm-device-plugin-"
+#define PLUGIN_PATH_TAG "NMManager-plugin-path"
+
+struct read_device_factory_paths_data {
+	char *path;
+	struct stat st;
+};
+
+static gint
+read_device_factory_paths_sort_fcn (gconstpointer a, gconstpointer b)
+{
+	const struct read_device_factory_paths_data *da = a;
+	const struct read_device_factory_paths_data *db = b;
+	time_t ta, tb;
+
+	ta = MAX (da->st.st_mtime, da->st.st_ctime);
+	tb = MAX (db->st.st_mtime, db->st.st_ctime);
+
+	if (ta < tb)
+		return 1;
+	if (ta > tb)
+		return -1;
+	return 0;
+}
+
+static char**
+read_device_factory_paths (void)
+{
+	GDir *dir;
+	GError *error = NULL;
+	const char *item;
+	GArray *paths;
+	char **result;
+	guint i;
+
+	dir = g_dir_open (NMPLUGINDIR, 0, &error);
+	if (!dir) {
+		nm_log_warn (LOGD_HW, "device plugin: failed to open directory %s: %s",
+		             NMPLUGINDIR,
+		             (error && error->message) ? error->message : "(unknown)");
+		g_clear_error (&error);
+		return NULL;
+	}
+
+	paths = g_array_new (FALSE, FALSE, sizeof (struct read_device_factory_paths_data));
+
+	while ((item = g_dir_read_name (dir))) {
+		int errsv;
+		struct read_device_factory_paths_data data;
+
+		if (!g_str_has_prefix (item, PLUGIN_PREFIX))
+			continue;
+		if (g_str_has_suffix (item, ".la"))
+			continue;
+
+		data.path = g_build_filename (NMPLUGINDIR, item, NULL);
+
+		if (stat (data.path, &data.st) != 0) {
+			errsv = errno;
+			nm_log_warn (LOGD_HW, "device plugin: skip invalid file %s (error during stat: %s)", data.path, strerror (errsv));
+			goto NEXT;
+		}
+		if (!S_ISREG (data.st.st_mode))
+			goto NEXT;
+		if (data.st.st_uid != 0) {
+			nm_log_warn (LOGD_HW, "device plugin: skip invalid file %s (file must be owned by root)", data.path);
+			goto NEXT;
+		}
+		if (data.st.st_mode & (S_IWGRP | S_IWOTH | S_ISUID)) {
+			nm_log_warn (LOGD_HW, "device plugin: skip invalid file %s (invalid file permissions)", data.path);
+			goto NEXT;
+		}
+
+		g_array_append_val (paths, data);
+		continue;
+NEXT:
+		g_free (data.path);
+	}
+	g_dir_close (dir);
+
+	/* sort filenames by modification time. */
+	g_array_sort (paths, read_device_factory_paths_sort_fcn);
+
+	result = g_new (char *, paths->len + 1);
+	for (i = 0; i < paths->len; i++)
+		result[i] = g_array_index (paths, struct read_device_factory_paths_data, i).path;
+	result[i] = NULL;
+
+	g_array_free (paths, TRUE);
+	return result;
+}
+
+static gboolean
+_add_factory (NMDeviceFactory *factory,
+              gboolean check_duplicates,
+              const char *path,
+              NMDeviceFactoryManagerFactoryLoadedFunc loaded_func,
+              gpointer user_data)
+{
+	NMDeviceFactory *found = NULL;
+	const NMLinkType *link_types = NULL;
+	const char **setting_types = NULL;
+	int i;
+
+	nm_device_factory_get_supported_types (factory, &link_types, &setting_types);
+	if (check_duplicates) {
+		found = nm_device_factory_manager_find_factory (link_types, setting_types);
+		if (found) {
+			nm_log_warn (LOGD_HW, "Loading device plugin failed: multiple plugins "
+			             "for same type (using '%s' instead of '%s')",
+				         (char *) g_object_get_data (G_OBJECT (found), PLUGIN_PATH_TAG),
+				         path);
+			return FALSE;
+		}
+	}
+
+	g_object_set_data_full (G_OBJECT (factory), PLUGIN_PATH_TAG, g_strdup (path), g_free);
+	for (i = 0; link_types[i] > NM_LINK_TYPE_UNKNOWN; i++)
+		g_hash_table_insert (factories_by_link, GUINT_TO_POINTER (link_types[i]), g_object_ref (factory));
+	for (i = 0; setting_types[i]; i++)
+		g_hash_table_insert (factories_by_setting, (char *) setting_types[i], g_object_ref (factory));
+
+	loaded_func (factory, user_data);
+
+	nm_log_info (LOGD_HW, "Loaded device plugin: %s (%s)", G_OBJECT_TYPE_NAME (factory), path);
+	return TRUE;
+}
+
+void
+nm_device_factory_manager_load_factories (NMDeviceFactoryManagerFactoryLoadedFunc loaded_func,
+                                          gpointer user_data)
+{
+	NMDeviceFactory *factory;
+	const GSList *iter;
+	GError *error = NULL;
+	char **path, **paths;
+
+	g_return_val_if_fail (factories_by_link == NULL, FALSE);
+	g_return_val_if_fail (factories_by_setting == NULL, FALSE);
+	factories_by_link = g_hash_table_new_full (g_direct_hash, g_direct_equal, NULL, g_object_unref);
+	factories_by_setting = g_hash_table_new_full (g_str_hash, g_str_equal, NULL, g_object_unref);
+
+	/* Register internal factories first */
+	for (iter = internal_types; iter; iter = iter->next) {
+		GType ftype = (GType) GPOINTER_TO_SIZE (iter->data);
+
+		factory = (NMDeviceFactory *) g_object_new (ftype, NULL);
+		g_assert (factory);
+		_add_factory (factory, FALSE, "internal", loaded_func, user_data);
+	}
+
+	paths = read_device_factory_paths ();
+	if (!paths)
+		return;
+
+	for (path = paths; *path; path++) {
+		GModule *plugin;
+		NMDeviceFactoryCreateFunc create_func;
+		const char *item;
+
+		item = strrchr (*path, '/');
+		g_assert (item);
+
+		plugin = g_module_open (*path, G_MODULE_BIND_LOCAL);
+
+		if (!plugin) {
+			nm_log_warn (LOGD_HW, "(%s): failed to load plugin: %s", item, g_module_error ());
+			continue;
+		}
+
+		if (!g_module_symbol (plugin, "nm_device_factory_create", (gpointer) &create_func)) {
+			nm_log_warn (LOGD_HW, "(%s): failed to find device factory creator: %s", item, g_module_error ());
+			g_module_close (plugin);
+			continue;
+		}
+
+		factory = create_func (&error);
+		if (!factory) {
+			nm_log_warn (LOGD_HW, "(%s): failed to initialize device factory: %s",
+			             item, error ? error->message : "unknown");
+			g_clear_error (&error);
+			g_module_close (plugin);
+			continue;
+		}
+		g_clear_error (&error);
+
+		if (_add_factory (factory, TRUE, g_module_name (plugin), loaded_func, user_data))
+			g_module_make_resident (plugin);
+		else
+			g_module_close (plugin);
+
+		g_object_unref (factory);
+	}
+	g_strfreev (paths);
 }
 