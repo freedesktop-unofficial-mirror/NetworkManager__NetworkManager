sujithshankar/NetworkManager

config: move keyfile values to NMConfigData

src/settings/plugins/ifnet/plugin.c
@@ -123,9 +123,9 @@ is_managed_plugin (void)
 {
 	char *result = NULL;
 
-	result = nm_config_get_value (nm_config_get (),
-	                              IFNET_KEY_FILE_GROUP, IFNET_KEY_FILE_KEY_MANAGED,
-	                              NULL);
+	result = nm_config_data_get_value (nm_config_get_data0 (nm_config_get ()),
+	                                   IFNET_KEY_FILE_GROUP, IFNET_KEY_FILE_KEY_MANAGED,
+	                                   NULL);
 	if (result) {
 		gboolean ret = is_true (result);
 		g_free (result);
@@ -264,9 +264,9 @@ reload_connections (NMSystemConfigInterface *config)
 
 	nm_log_info (LOGD_SETTINGS, "Loading connections");
 
-	str_auto_refresh = nm_config_get_value (nm_config_get (),
-	                                        IFNET_KEY_FILE_GROUP, "auto_refresh",
-	                                        NULL);
+	str_auto_refresh = nm_config_data_get_value (nm_config_get_data0 (nm_config_get ()),
+	                                             IFNET_KEY_FILE_GROUP, "auto_refresh",
+	                                             NULL);
 	if (str_auto_refresh && is_true (str_auto_refresh))
 		auto_refresh = TRUE;
 	g_free (str_auto_refresh);

src/settings/plugins/ifupdown/plugin.c
@@ -456,9 +456,9 @@ SCPluginIfupdown_init (NMSystemConfigInterface *config)
 	g_hash_table_destroy (auto_ifaces);
 
 	/* Check the config file to find out whether to manage interfaces */
-	value = nm_config_get_value (nm_config_get (),
-	                             IFUPDOWN_KEY_FILE_GROUP, IFUPDOWN_KEY_FILE_KEY_MANAGED,
-	                             &error);
+	value = nm_config_data_get_value (nm_config_get_data0 (nm_config_get ()),
+	                                  IFUPDOWN_KEY_FILE_GROUP, IFUPDOWN_KEY_FILE_KEY_MANAGED,
+	                                  &error);
 	if (error) {
 		nm_log_info (LOGD_SETTINGS, "loading system config file (%s) caused error: %s",
 		             nm_config_data_get_config_main_file (nm_config_get_data (nm_config_get ())),

src/tests/config/test-config.c
@@ -104,16 +104,16 @@ test_config_simple (void)
 	g_assert_cmpstr (plugins[1], ==, "bar");
 	g_assert_cmpstr (plugins[2], ==, "baz");
 
-	value = nm_config_get_value (config, "extra-section", "extra-key", NULL);
+	value = nm_config_data_get_value (nm_config_get_data0 (config), "extra-section", "extra-key", NULL);
 	g_assert_cmpstr (value, ==, "some value");
 	g_free (value);
 
-	value = nm_config_get_value (config, "extra-section", "no-key", &error);
+	value = nm_config_data_get_value (nm_config_get_data0 (config), "extra-section", "no-key", &error);
 	g_assert (!value);
 	g_assert_error (error, G_KEY_FILE_ERROR, G_KEY_FILE_ERROR_KEY_NOT_FOUND);
 	g_clear_error (&error);
 
-	value = nm_config_get_value (config, "no-section", "no-key", &error);
+	value = nm_config_data_get_value (nm_config_get_data0 (config), "no-section", "no-key", &error);
 	g_assert (!value);
 	g_assert_error (error, G_KEY_FILE_ERROR, G_KEY_FILE_ERROR_GROUP_NOT_FOUND);
 	g_clear_error (&error);
@@ -248,21 +248,21 @@ test_config_confdir (void)
 	g_assert_cmpstr (plugins[3], ==, "one");
 	g_assert_cmpstr (plugins[4], ==, "two");
 
-	value = nm_config_get_value (config, "main", "extra", NULL);
+	value = nm_config_data_get_value (nm_config_get_data0 (config), "main", "extra", NULL);
 	g_assert_cmpstr (value, ==, "hello");
 	g_free (value);
 
-	value = nm_config_get_value (config, "main", "new", NULL);
+	value = nm_config_data_get_value (nm_config_get_data0 (config), "main", "new", NULL);
 	g_assert_cmpstr (value, ==, "something"); /* not ",something" */
 	g_free (value);
 
-	value = nm_config_get_value (config, "order", "a", NULL);
+	value = nm_config_data_get_value (nm_config_get_data0 (config), "order", "a", NULL);
 	g_assert_cmpstr (value, ==, "90");
 	g_free (value);
-	value = nm_config_get_value (config, "order", "b", NULL);
+	value = nm_config_data_get_value (nm_config_get_data0 (config), "order", "b", NULL);
 	g_assert_cmpstr (value, ==, "10");
 	g_free (value);
-	value = nm_config_get_value (config, "order", "c", NULL);
+	value = nm_config_data_get_value (nm_config_get_data0 (config), "order", "c", NULL);
 	g_assert_cmpstr (value, ==, "0");
 	g_free (value);
 

freedesktop-unofficial-mirror/NetworkManager__NetworkManager

src/settings/plugins/ifnet/plugin.c
@@ -123,9 +123,9 @@ is_managed_plugin (void)
 {
 	char *result = NULL;
 
-	result = nm_config_get_value (nm_config_get (),
-	                              IFNET_KEY_FILE_GROUP, IFNET_KEY_FILE_KEY_MANAGED,
-	                              NULL);
+	result = nm_config_data_get_value (nm_config_get_data_orig (nm_config_get ()),
+	                                   IFNET_KEY_FILE_GROUP, IFNET_KEY_FILE_KEY_MANAGED,
+	                                   NULL);
 	if (result) {
 		gboolean ret = is_true (result);
 		g_free (result);
@@ -264,9 +264,9 @@ reload_connections (NMSystemConfigInterface *config)
 
 	nm_log_info (LOGD_SETTINGS, "Loading connections");
 
-	str_auto_refresh = nm_config_get_value (nm_config_get (),
-	                                        IFNET_KEY_FILE_GROUP, "auto_refresh",
-	                                        NULL);
+	str_auto_refresh = nm_config_data_get_value (nm_config_get_data_orig (nm_config_get ()),
+	                                             IFNET_KEY_FILE_GROUP, "auto_refresh",
+	                                             NULL);
 	if (str_auto_refresh && is_true (str_auto_refresh))
 		auto_refresh = TRUE;
 	g_free (str_auto_refresh);

src/settings/plugins/ifupdown/plugin.c
@@ -456,9 +456,9 @@ SCPluginIfupdown_init (NMSystemConfigInterface *config)
 	g_hash_table_destroy (auto_ifaces);
 
 	/* Check the config file to find out whether to manage interfaces */
-	value = nm_config_get_value (nm_config_get (),
-	                             IFUPDOWN_KEY_FILE_GROUP, IFUPDOWN_KEY_FILE_KEY_MANAGED,
-	                             &error);
+	value = nm_config_data_get_value (nm_config_get_data_orig (nm_config_get ()),
+	                                  IFUPDOWN_KEY_FILE_GROUP, IFUPDOWN_KEY_FILE_KEY_MANAGED,
+	                                  &error);
 	if (error) {
 		nm_log_info (LOGD_SETTINGS, "loading system config file (%s) caused error: %s",
 		             nm_config_data_get_config_main_file (nm_config_get_data (nm_config_get ())),

src/tests/config/test-config.c
@@ -108,16 +108,16 @@ test_config_simple (void)
 	g_assert_cmpstr (plugins[1], ==, "bar");
 	g_assert_cmpstr (plugins[2], ==, "baz");
 
-	value = nm_config_get_value (config, "extra-section", "extra-key", NULL);
+	value = nm_config_data_get_value (nm_config_get_data_orig (config), "extra-section", "extra-key", NULL);
 	g_assert_cmpstr (value, ==, "some value");
 	g_free (value);
 
-	value = nm_config_get_value (config, "extra-section", "no-key", &error);
+	value = nm_config_data_get_value (nm_config_get_data_orig (config), "extra-section", "no-key", &error);
 	g_assert (!value);
 	g_assert_error (error, G_KEY_FILE_ERROR, G_KEY_FILE_ERROR_KEY_NOT_FOUND);
 	g_clear_error (&error);
 
-	value = nm_config_get_value (config, "no-section", "no-key", &error);
+	value = nm_config_data_get_value (nm_config_get_data_orig (config), "no-section", "no-key", &error);
 	g_assert (!value);
 	g_assert_error (error, G_KEY_FILE_ERROR, G_KEY_FILE_ERROR_GROUP_NOT_FOUND);
 	g_clear_error (&error);
@@ -252,21 +252,21 @@ test_config_confdir (void)
 	g_assert_cmpstr (plugins[3], ==, "one");
 	g_assert_cmpstr (plugins[4], ==, "two");
 
-	value = nm_config_get_value (config, "main", "extra", NULL);
+	value = nm_config_data_get_value (nm_config_get_data_orig (config), "main", "extra", NULL);
 	g_assert_cmpstr (value, ==, "hello");
 	g_free (value);
 
-	value = nm_config_get_value (config, "main", "new", NULL);
+	value = nm_config_data_get_value (nm_config_get_data_orig (config), "main", "new", NULL);
 	g_assert_cmpstr (value, ==, "something"); /* not ",something" */
 	g_free (value);
 
-	value = nm_config_get_value (config, "order", "a", NULL);
+	value = nm_config_data_get_value (nm_config_get_data_orig (config), "order", "a", NULL);
 	g_assert_cmpstr (value, ==, "90");
 	g_free (value);
-	value = nm_config_get_value (config, "order", "b", NULL);
+	value = nm_config_data_get_value (nm_config_get_data_orig (config), "order", "b", NULL);
 	g_assert_cmpstr (value, ==, "10");
 	g_free (value);
-	value = nm_config_get_value (config, "order", "c", NULL);
+	value = nm_config_data_get_value (nm_config_get_data_orig (config), "order", "c", NULL);
 	g_assert_cmpstr (value, ==, "0");
 	g_free (value);
 