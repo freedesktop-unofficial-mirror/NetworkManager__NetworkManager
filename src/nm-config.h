sujithshankar/NetworkManager

config: add nm_config_setup() to initialize config singleton
Make nm_config_new() usable without accessing static/singleton data.
This will soon be useful when we support reloading the configuration
at runtime. Then we want to create yet another NMConfig instance
that we merge into the existing singleton.

nm_config_setup() is now used to initialize the singleton.
Still, you must not call nm_config_get() before calling
nm_config_setup() or after freeing the provided singleton
instance.

Signed-off-by: Thomas Haller <thaller@redhat.com>

src/nm-config.h
@@ -36,6 +36,8 @@ G_BEGIN_DECLS
 #define NM_IS_CONFIG_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass),  NM_TYPE_CONFIG))
 #define NM_CONFIG_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj),  NM_TYPE_CONFIG, NMConfigClass))
 
+typedef struct _NMConfigCmdLineOptions NMConfigCmdLineOptions;
+
 typedef struct {
 	GObject parent;
 } NMConfig;
@@ -69,8 +71,13 @@ gboolean nm_config_get_ignore_carrier (NMConfig *config, NMDevice *device);
 char *nm_config_get_value (NMConfig *config, const char *group, const char *key, GError **error);
 
 /* for main.c only */
-GOptionEntry *nm_config_get_options (void);
-NMConfig *nm_config_new (GError **error);
+NMConfigCmdLineOptions *nm_config_cmd_line_options_new (void);
+void                    nm_config_cmd_line_options_free (NMConfigCmdLineOptions *cli);
+void                    nm_config_cmd_line_options_add_to_entries (NMConfigCmdLineOptions *cli,
+                                                                   GOptionContext *opt_ctx);
+
+NMConfig *nm_config_new (const NMConfigCmdLineOptions *cli, GError **error);
+NMConfig *nm_config_setup (const NMConfigCmdLineOptions *cli, GError **error);
 
 G_END_DECLS
 

freedesktop-unofficial-mirror/NetworkManager__NetworkManager

src/nm-config.h
@@ -36,6 +36,8 @@ G_BEGIN_DECLS
 #define NM_IS_CONFIG_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass),  NM_TYPE_CONFIG))
 #define NM_CONFIG_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj),  NM_TYPE_CONFIG, NMConfigClass))
 
+typedef struct NMConfigCmdLineOptions NMConfigCmdLineOptions;
+
 struct _NMConfig {
 	GObject parent;
 };
@@ -71,8 +73,13 @@ gboolean nm_config_get_ignore_carrier (NMConfig *config, NMDevice *device);
 char *nm_config_get_value (NMConfig *config, const char *group, const char *key, GError **error);
 
 /* for main.c only */
-GOptionEntry *nm_config_get_options (void);
-NMConfig *nm_config_new (GError **error);
+NMConfigCmdLineOptions *nm_config_cmd_line_options_new (void);
+void                    nm_config_cmd_line_options_free (NMConfigCmdLineOptions *cli);
+void                    nm_config_cmd_line_options_add_to_entries (NMConfigCmdLineOptions *cli,
+                                                                   GOptionContext *opt_ctx);
+
+NMConfig *nm_config_new (const NMConfigCmdLineOptions *cli, GError **error);
+NMConfig *nm_config_setup (const NMConfigCmdLineOptions *cli, GError **error);
 
 G_END_DECLS
 