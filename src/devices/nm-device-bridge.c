sujithshankar/NetworkManager

core: let device plugins advertise supported link and setting types
Instead of looping over all plugins and asking each plugin whether it
can handle a link or a connection, have them advertise the link and
connection types they support, and use that when creating new devices.

src/devices/nm-device-bridge.c
@@ -552,7 +552,9 @@ create_virtual_device_for_connection (NMDeviceFactory *factory,
 	                                  NULL);
 }
 
-DEFINE_DEVICE_FACTORY_INTERNAL(BRIDGE, Bridge, bridge,
+DEFINE_DEVICE_FACTORY_INTERNAL      (BRIDGE, Bridge, bridge,
+	DEFINE_FACTORY_LINK_TYPES    (NM_LINK_TYPE_BRIDGE)
+	DEFINE_FACTORY_SETTING_TYPES (NM_SETTING_BRIDGE_SETTING_NAME),
 	factory_iface->new_link = new_link;
 	factory_iface->create_virtual_device_for_connection = create_virtual_device_for_connection;
 	)

src/devices/nm-device-ethernet.c
@@ -1765,7 +1765,9 @@ new_link (NMDeviceFactory *factory, NMPlatformLink *plink, GError **error)
 	return NULL;
 }
 
-DEFINE_DEVICE_FACTORY_INTERNAL(ETHERNET, Ethernet, ethernet, \
-	factory_iface->new_link = new_link; \
+DEFINE_DEVICE_FACTORY_INTERNAL      (ETHERNET, Ethernet, ethernet,
+	DEFINE_FACTORY_LINK_TYPES    (NM_LINK_TYPE_ETHERNET)
+	DEFINE_FACTORY_SETTING_TYPES (NM_SETTING_WIRED_SETTING_NAME, NM_SETTING_PPPOE_SETTING_NAME),
+	factory_iface->new_link = new_link;
 	)
 

src/devices/nm-device-infiniband.c
@@ -356,8 +356,10 @@ create_virtual_device_for_connection (NMDeviceFactory *factory,
 	                                  NULL);
 }
 
-DEFINE_DEVICE_FACTORY_INTERNAL(INFINIBAND, Infiniband, infiniband, \
-	factory_iface->new_link = new_link; \
+DEFINE_DEVICE_FACTORY_INTERNAL      (INFINIBAND, Infiniband, infiniband,
+	DEFINE_FACTORY_LINK_TYPES    (NM_LINK_TYPE_INFINIBAND)
+	DEFINE_FACTORY_SETTING_TYPES (NM_SETTING_INFINIBAND_SETTING_NAME),
+	factory_iface->new_link = new_link;
 	factory_iface->create_virtual_device_for_connection = create_virtual_device_for_connection;
 	)
 

src/devices/nm-device-tun.c
@@ -289,7 +289,8 @@ new_link (NMDeviceFactory *factory, NMPlatformLink *plink, GError **error)
 	                                  NULL);
 }
 
-DEFINE_DEVICE_FACTORY_INTERNAL_WITH_DEVTYPE(TUN, Tun, tun, GENERIC, \
-	factory_iface->new_link = new_link; \
+DEFINE_DEVICE_FACTORY_INTERNAL      (TUN, Tun, tun,
+	DEFINE_FACTORY_LINK_TYPES    (NM_LINK_TYPE_TUN, NM_LINK_TYPE_TAP),
+	factory_iface->new_link = new_link;
 	)
 

src/devices/nm-device-veth.c
@@ -178,7 +178,8 @@ new_link (NMDeviceFactory *factory, NMPlatformLink *plink, GError **error)
 	return NULL;
 }
 
-DEFINE_DEVICE_FACTORY_INTERNAL_WITH_DEVTYPE(VETH, Veth, veth, ETHERNET, \
-	factory_iface->new_link = new_link; \
+DEFINE_DEVICE_FACTORY_INTERNAL      (VETH, Veth, veth,
+	DEFINE_FACTORY_LINK_TYPES    (NM_LINK_TYPE_VETH),
+	factory_iface->new_link = new_link;
 	)
 

src/devices/nm-device-vlan.c
@@ -692,8 +692,10 @@ create_virtual_device_for_connection (NMDeviceFactory *factory,
 	return device;
 }
 
-DEFINE_DEVICE_FACTORY_INTERNAL(VLAN, Vlan, vlan, \
-	factory_iface->new_link = new_link; \
+DEFINE_DEVICE_FACTORY_INTERNAL      (VLAN, Vlan, vlan,
+	DEFINE_FACTORY_LINK_TYPES    (NM_LINK_TYPE_VLAN)
+	DEFINE_FACTORY_SETTING_TYPES (NM_SETTING_VLAN_SETTING_NAME),
+	factory_iface->new_link = new_link;
 	factory_iface->create_virtual_device_for_connection = create_virtual_device_for_connection;
 	)
 

src/devices/nm-device-vxlan.c
@@ -365,7 +365,8 @@ new_link (NMDeviceFactory *factory, NMPlatformLink *plink, GError **error)
 	return NULL;
 }
 
-DEFINE_DEVICE_FACTORY_INTERNAL_WITH_DEVTYPE(VXLAN, Vxlan, vxlan, GENERIC, \
-	factory_iface->new_link = new_link; \
+DEFINE_DEVICE_FACTORY_INTERNAL      (VXLAN, Vxlan, vxlan,
+	DEFINE_FACTORY_LINK_TYPES    (NM_LINK_TYPE_VXLAN),
+	factory_iface->new_link = new_link;
 	)
 

freedesktop-unofficial-mirror/NetworkManager__NetworkManager

src/devices/nm-device-bridge.c
@@ -537,7 +537,9 @@ create_virtual_device_for_connection (NMDeviceFactory *factory,
 	                                  NULL);
 }
 
-DEFINE_DEVICE_FACTORY_INTERNAL(BRIDGE, Bridge, bridge,
+NM_DEVICE_FACTORY_DEFINE_INTERNAL (BRIDGE, Bridge, bridge,
+	NM_DEVICE_FACTORY_DECLARE_LINK_TYPES    (NM_LINK_TYPE_BRIDGE)
+	NM_DEVICE_FACTORY_DECLARE_SETTING_TYPES (NM_SETTING_BRIDGE_SETTING_NAME),
 	factory_iface->new_link = new_link;
 	factory_iface->create_virtual_device_for_connection = create_virtual_device_for_connection;
 	)

src/devices/nm-device-ethernet.c
@@ -1755,7 +1755,9 @@ new_link (NMDeviceFactory *factory, NMPlatformLink *plink, GError **error)
 	return NULL;
 }
 
-DEFINE_DEVICE_FACTORY_INTERNAL(ETHERNET, Ethernet, ethernet, \
-	factory_iface->new_link = new_link; \
+NM_DEVICE_FACTORY_DEFINE_INTERNAL (ETHERNET, Ethernet, ethernet,
+	NM_DEVICE_FACTORY_DECLARE_LINK_TYPES    (NM_LINK_TYPE_ETHERNET)
+	NM_DEVICE_FACTORY_DECLARE_SETTING_TYPES (NM_SETTING_WIRED_SETTING_NAME, NM_SETTING_PPPOE_SETTING_NAME),
+	factory_iface->new_link = new_link;
 	)
 

src/devices/nm-device-infiniband.c
@@ -344,8 +344,10 @@ create_virtual_device_for_connection (NMDeviceFactory *factory,
 	                                  NULL);
 }
 
-DEFINE_DEVICE_FACTORY_INTERNAL(INFINIBAND, Infiniband, infiniband, \
-	factory_iface->new_link = new_link; \
+NM_DEVICE_FACTORY_DEFINE_INTERNAL (INFINIBAND, Infiniband, infiniband,
+	NM_DEVICE_FACTORY_DECLARE_LINK_TYPES    (NM_LINK_TYPE_INFINIBAND)
+	NM_DEVICE_FACTORY_DECLARE_SETTING_TYPES (NM_SETTING_INFINIBAND_SETTING_NAME),
+	factory_iface->new_link = new_link;
 	factory_iface->create_virtual_device_for_connection = create_virtual_device_for_connection;
 	)
 

src/devices/nm-device-tun.c
@@ -289,7 +289,8 @@ new_link (NMDeviceFactory *factory, NMPlatformLink *plink, GError **error)
 	                                  NULL);
 }
 
-DEFINE_DEVICE_FACTORY_INTERNAL_WITH_DEVTYPE(TUN, Tun, tun, GENERIC, \
-	factory_iface->new_link = new_link; \
+NM_DEVICE_FACTORY_DEFINE_INTERNAL (TUN, Tun, tun,
+	NM_DEVICE_FACTORY_DECLARE_LINK_TYPES    (NM_LINK_TYPE_TUN, NM_LINK_TYPE_TAP),
+	factory_iface->new_link = new_link;
 	)
 

src/devices/nm-device-veth.c
@@ -189,7 +189,8 @@ new_link (NMDeviceFactory *factory, NMPlatformLink *plink, GError **error)
 	return NULL;
 }
 
-DEFINE_DEVICE_FACTORY_INTERNAL_WITH_DEVTYPE(VETH, Veth, veth, ETHERNET, \
-	factory_iface->new_link = new_link; \
+NM_DEVICE_FACTORY_DEFINE_INTERNAL (VETH, Veth, veth,
+	NM_DEVICE_FACTORY_DECLARE_LINK_TYPES    (NM_LINK_TYPE_VETH),
+	factory_iface->new_link = new_link;
 	)
 

src/devices/nm-device-vlan.c
@@ -749,8 +749,10 @@ create_virtual_device_for_connection (NMDeviceFactory *factory,
 	return device;
 }
 
-DEFINE_DEVICE_FACTORY_INTERNAL(VLAN, Vlan, vlan, \
-	factory_iface->new_link = new_link; \
+NM_DEVICE_FACTORY_DEFINE_INTERNAL (VLAN, Vlan, vlan,
+	NM_DEVICE_FACTORY_DECLARE_LINK_TYPES    (NM_LINK_TYPE_VLAN)
+	NM_DEVICE_FACTORY_DECLARE_SETTING_TYPES (NM_SETTING_VLAN_SETTING_NAME),
+	factory_iface->new_link = new_link;
 	factory_iface->create_virtual_device_for_connection = create_virtual_device_for_connection;
 	)
 

src/devices/nm-device-vxlan.c
@@ -365,7 +365,8 @@ new_link (NMDeviceFactory *factory, NMPlatformLink *plink, GError **error)
 	return NULL;
 }
 
-DEFINE_DEVICE_FACTORY_INTERNAL_WITH_DEVTYPE(VXLAN, Vxlan, vxlan, GENERIC, \
-	factory_iface->new_link = new_link; \
+NM_DEVICE_FACTORY_DEFINE_INTERNAL (VXLAN, Vxlan, vxlan,
+	NM_DEVICE_FACTORY_DECLARE_LINK_TYPES    (NM_LINK_TYPE_VXLAN),
+	factory_iface->new_link = new_link;
 	)
 