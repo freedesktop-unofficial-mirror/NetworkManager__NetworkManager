315d1d815bc3bad473bed18a74a5493532e9c45d
Dan Williams <dcbw@redhat.com>
2015-04-20 10:18:32 -0500

    wifi: remove supplicant-forgotten current AP from list on disconnect
    NM never removes the current AP from the AP list, to prevent NM from
    indicating that it's connected, but to nothing.  But the supplicant
    can remove that AP from its list at any time (out of range, turned off,
    etc), leading to a priv->current_ap that is no longer known to the
    supplicant but still exists in the NM AP list.  Since the supplicant
    has forgotten it, NM will never receive a removal signal for it.
    
    To ensure that a supplicant-forgotten priv->current_ap is removed
    from the NM AP list when priv->current_ap is cleared or changed, mark
    any AP removed by the supplicant as 'fake'.  It will then always be
    removed in set_current_ap() and not linger in the AP list forever like
    a zombie.

--- src/devices/wifi/nm-wifi-ap.c
+++ src/devices/wifi/nm-wifi-ap.c
@@ -374,6 +374,7 @@ nm_ap_update_from_properties (NMAccessPoint *ap,
                               const char *supplicant_path,
                               GVariant *properties)
 {
+	NMAccessPointPrivate *priv;
 	char *addr;
 	const guint8 *bytes;
 	GVariant *v;
@@ -385,6 +386,7 @@ nm_ap_update_from_properties (NMAccessPoint *ap,
 
 	g_return_if_fail (ap != NULL);
 	g_return_if_fail (properties != NULL);
+	priv = NM_AP_GET_PRIVATE (ap);
 
 	g_object_freeze_notify (G_OBJECT (ap));
 
@@ -461,6 +463,7 @@ nm_ap_update_from_properties (NMAccessPoint *ap,
 		nm_ap_set_supplicant_path (ap, supplicant_path);
 
 	nm_ap_set_last_seen (ap, nm_utils_get_monotonic_timestamp_s ());
+	priv->fake = FALSE;
 
 	g_object_thaw_notify (G_OBJECT (ap));
 }
